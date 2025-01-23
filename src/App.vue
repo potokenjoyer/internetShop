9787930cf75d74c4f42bfb9ad47f77f5f30c8c7b
potokenjoyer <potokenjoyer@gmail.com>
2025-01-23 18:00:27 +0300

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
+import Header from "./components/Header.vue";
 </script>
 
 <template>
+  <Header/>
   <router-view></router-view>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <div class="navigation">
+      <h1 style="margin-left: 5%">logotip</h1>
+      <a @click="$router.push('/')">КАТАЛОГ</a>
+      <a @click="$router.push('/about')">О НАС</a>
+      <a @click="$router.push('/order')">КОРЗИНА</a>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.header {
+  width: 100%;
+  display: flex;
+  height: 100%;
+  border-bottom: 1px solid  black;
+}
+.navigation{
+display: flex;
+width: 40%;
+height: 100%;
+align-items: center;
+justify-content: space-between;
+font-size: 18px;
+}
+
+</style>
